a83a845c35645616a56ad6c6b347b50b8fdd4a2a
Vikash Sharma <167905556+vikuuuuu@users.noreply.github.com>
2026-02-17 15:56:57 +0530

    Remove ViewCounter route from AppLayout
    Removed the ViewCounter route from the AppLayout component.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Menu from "./Component/Menu";
 import Home from "./Component/Home";
-import ViewCounter from "./Component/ViewCounter";
 
 function AppLayout() {
   const location = useLocation();
@@ -14,7 +13,7 @@ function AppLayout() {
       <div className={`AppContent ${isHomePage ? "homeLayout" : "fullLayout"}`}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/viewcounter" element={<ViewCounter />} />
+    
         </Routes>
       </div>
     </>
